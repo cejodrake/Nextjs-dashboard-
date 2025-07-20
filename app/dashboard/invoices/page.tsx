cc0b52d4669cfa99ff25121a572bcc14416d6bcc
cejodrake <j_calix2002@hotmail.com>
2025-07-20 12:25:53 -0700

    Adding the pagination functionality

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,20 +1,24 @@
 import Pagination from "@/app/ui/invoices/pagination"
 import Search from "@/app/ui/search"
 import Table from "@/app/ui/invoices/table"
-import { CreateInvoice } from "@/app/ui/invoices/buttons"
 import { lusitana } from "@/app/ui/fonts"
+import { CreateInvoice } from "@/app/ui/invoices/buttons"
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { fetchInvoicesPages } from '@/app/lib/data';
+ 
 import { Suspense } from "react"
 
 export default async function Page(props:{
     searchParams?:Promise<{
         query?:string;
-        page?:string
+        page?:string,
+
     }>
 }) {
     const searchParams = await props.searchParams;
     const query = searchParams?.query || '';
     const currentPage = Number(searchParams?.page) || 1;
+    const totalPages = await  fetchInvoicesPages(query);
     return (
         <div className="w-full">
             <div className="flex w-full items-center justify-beteween">
@@ -28,7 +32,7 @@ export default async function Page(props:{
                 <Table query={query} currentPage={currentPage} />
             </Suspense>}
             <div className="mt-5 flex w-full justify-center">
-                {/* <Pagination totalPages={totalPages} /> */}
+                <Pagination totalPages={totalPages} />
             </div>
         </div>
 
